747abc437865fb2a0490033416476160d9b40c8e
MTrucky <146337304+MTrucky@users.noreply.github.com>
2024-02-20 22:09:28 +0500

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,8 +1,6 @@
-n = 0
-for x in 'XYZ':
-    for y in 'ABCD':
-        for z in 'ABCD':
-            for w in 'ABCD':
-                s = x + y + z + w
-                n+=1
+from itertools import product
+
+n =  0
+for s in product('XYZ', 'ABCD', 'ABCD', 'ABCD'):
+    n +=  1
 print(n)
