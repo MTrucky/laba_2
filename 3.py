e2b08b4abaf79c78f91571bf12cd4c98bbc5154b
MTrucky <146337304+MTrucky@users.noreply.github.com>
2024-02-20 22:08:15 +0500

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,23 +1,22 @@
-def f1(x):
-     while x%2==0:
-          x=x//2
-     if (x**0.25) == int(x**0.25):return True
-     else:return False
-def f(x):
-     k=2
-     deliteli=set()
-     if x%2!=0:deliteli.add(x)
-     deliteli.add(1)
-     while k*k<=x:
-          if x%k==0:
-               if k%2!=0:deliteli.add(k)
-               if x//k<x:
-                    if (x//k)%2!=0:deliteli.add(x//k)
-          k=k+1
-     return sorted(deliteli)
-start=45000000
-end=50000000
-numbers=[int(x) for x in range(start,end+1) if f1(x)==True]
-for i in numbers:
-     if len(f(i))==5:
-          print(i)
+def isOddNumberPrime(n):
+  for x in range(3, n, 2):
+    if x*x > n:
+      return True
+    if n % x == 0:
+      return False
+
+p = [x for x in range(3, 100, 2) if isOddNumberPrime(x)]
+p4s = { x**4 for x in p }
+
+l = 45000000
+r = 50000000
+res = []
+
+for n in range(l, r+1):
+  x = n
+  while (x & 1) == 0:
+    x >>= 1
+  if x in p4s:
+    res.append(n)
+
+print(res)
